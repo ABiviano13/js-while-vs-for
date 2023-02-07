ac9851367d4b8b27abc0658ccb71352db973350e
ABiviano13 <abiviano0@outlook.com>
2023-02-07 15:55:54 +0100

    FEAT array + if-else

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// 1. Per ricavare i numeri dispari dall'utente useremo il ciclo while
+// 1.1 Creiamo un array vuoto dove salveremo i nostri numeri dispari
+let numeriDispari = [];
+console.log(numeriDispari)
+// 1.2 Creiamo una variabile numero
+let numeroUtente = parseInt(prompt('Inserisci un numero a piacere'));
+// // 1.3 Creiamo una variabile resto
+resto = numeroUtente % 2;
+    console.log(numeroUtente, resto) 
+// 1.4 Creiamo una variabile soglia (10 numeri dispari)
+let soglia = 10 * numeroUtente;
+// 1.5 Creiamo un - if else -
+if (resto === 0  || isNaN(numeroUtente)){
+    console.log('scarto')
+}else {
+    numeriDispari.push()
+}
+// 1.6 Creiamo un ciclo - while -
+
+
+
+
+
+// 2. Per stampare i numeri dispari in console a uno a uno useremo il ciclo for
