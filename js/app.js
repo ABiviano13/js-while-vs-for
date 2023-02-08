f61832778f22bcbe7efbb2c9c2e13e6fbe5645f2
ABiviano13 <abiviano0@outlook.com>
2023-02-08 09:55:40 +0100

    CORRECT - REFACTORING

--- js/app.js
+++ js/app.js
@@ -2,9 +2,7 @@
 // 1.1 Creiamo un array vuoto dove salveremo i nostri numeri dispari
 let numeriDispari = [];
 console.log(numeriDispari);
-let i = 0;
 
-let indiceNumeriDispari = i;
 // // 1.2 Creiamo una variabile numero
 let numeroUtente = ''
 
@@ -12,22 +10,22 @@ let numeroUtente = ''
 do{
     // 1.3.1 Creiamo una variabile numero
     numeroUtente = parseInt(prompt('Inserisci un numero a piacere'));
+
     // // 1.3.2 Creiamo una variabile resto
-    resto = numeroUtente % 2;
+    let resto = numeroUtente % 2;
     // console.log(numeroUtente, resto);
     
     // 1.3.3 Creiamo un - if else -
     if (resto !== 0  || isNaN(numeroUtente)){
         numeriDispari.push(numeroUtente);
-        indiceNumeriDispari += 1;
+
         // 2. Stampare a uno a uno i numeri dispari nella console
         console.log(numeroUtente);
-        // console.log(indiceNumeriDispari);
     }else {
         console.log('scarto');
     };
 
-} while(indiceNumeriDispari !== 10);
+} while(numeriDispari.length < 10);
 
 console.log('finalmente usciti dal ciclo while');
 
